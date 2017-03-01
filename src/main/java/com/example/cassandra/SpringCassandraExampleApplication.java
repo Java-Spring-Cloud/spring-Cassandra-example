3f36d08eb4c7b909e6681529c8c3cceb1e22dd3a
Anjan Jakkireddy <anjana.reddy13@t-mobile.com>
2017-03-01 16:50:33 -0500

    Initial Draft

---
+++ src/main/java/com/example/cassandra/SpringCassandraExampleApplication.java
@@ -0,0 +1,12 @@
+package com.example.cassandra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCassandraExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCassandraExampleApplication.class, args);
+	}
+}
